d06b23d2c777aa87ae187dcb46f326b7797284b2
syed <syed@syed>
2013-02-13 23:30:48 +0530

    new files

---
+++ src/accounts/app/bean/backingBean/CompStakeholderBean.java
@@ -0,0 +1,136 @@
+package accounts.app.bean.backingBean;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+
+import org.primefaces.model.LazyDataModel;
+
+import accounts.app.service.CompanyService;
+import accounts.model.entity.StakeHolder;
+
+@ManagedBean
+@SessionScoped
+public class CompStakeholderBean  implements Serializable{
+	private CompanyService companyService;
+	private List<StakeHolder> stakeHolder;
+	private StakeHolder selectedStakeHolder;
+	private LazyDataModel<StakeHolder> lazyModel;  
+
+	private String stakeHoldName;
+
+	private Date stakeHoldDob;
+
+	
+
+	private int stakeHoldPhNo;
+
+	private String stakeHoldAddr;
+
+	private String stakeHoldEmail;
+
+	private String stakeHoldPan;
+	 
+	
+	@Inject
+	public CompStakeholderBean(final CompanyService service, CompanyBean company)
+	{
+		this.companyService = service;
+		this.stakeHolder = companyService.getStakeHolders(company.getCompany());
+		this.lazyModel = new LazyCarDataModel(stakeHolder);
+
+	}
+
+	public CompanyService getCompanyService() {
+		return companyService;
+	}
+
+	public void setCompanyService(CompanyService companyService) {
+		this.companyService = companyService;
+	}
+
+	public StakeHolder getSelectedStakeHolder() {
+		return selectedStakeHolder;
+	}
+
+	public void setSelectedStakeHolder(StakeHolder selectedStakeHolder) {
+		this.selectedStakeHolder = selectedStakeHolder;
+	}
+
+	public String getStakeHoldName() {
+		return stakeHoldName;
+	}
+
+	public void setStakeHoldName(String stakeHoldName) {
+		this.stakeHoldName = stakeHoldName;
+	}
+
+	public Date getStakeHoldDob() {
+		return stakeHoldDob;
+	}
+
+	public void setStakeHoldDob(Date stakeHoldDob) {
+		this.stakeHoldDob = stakeHoldDob;
+	}
+
+	public int getStakeHoldPhNo() {
+		return stakeHoldPhNo;
+	}
+
+	public void setStakeHoldPhNo(int stakeHoldPhNo) {
+		this.stakeHoldPhNo = stakeHoldPhNo;
+	}
+
+	public String getStakeHoldAddr() {
+		return stakeHoldAddr;
+	}
+
+	public void setStakeHoldAddr(String stakeHoldAddr) {
+		this.stakeHoldAddr = stakeHoldAddr;
+	}
+
+	public String getStakeHoldEmail() {
+		return stakeHoldEmail;
+	}
+
+	public void setStakeHoldEmail(String stakeHoldEmail) {
+		this.stakeHoldEmail = stakeHoldEmail;
+	}
+
+	public String getStakeHoldPan() {
+		return stakeHoldPan;
+	}
+
+	public void setStakeHoldPan(String stakeHoldPan) {
+		this.stakeHoldPan = stakeHoldPan;
+	}
+	public List<StakeHolder> getStakeHolder() {
+		return stakeHolder;
+	}
+
+	public void setStakeHolder(List<StakeHolder> stakeHolder) {
+		this.stakeHolder = stakeHolder;
+	}
+
+	public LazyDataModel<StakeHolder> getLazyModel() {
+		return lazyModel;
+	}
+
+	public void setLazyModel(LazyDataModel<StakeHolder> lazyModel) {
+		this.lazyModel = lazyModel;
+	}
+
+	public StakeHolder getSelectedCar() {
+		return selectedStakeHolder;
+	}
+
+	public void setSelectedCar(StakeHolder selectedCar) {
+		this.selectedStakeHolder = selectedCar;
+	}
+
+}
---
+++ src/accounts/model/entity/StakeHolder.java
@@ -0,0 +1,105 @@
+package accounts.model.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+
+
+@Entity
+@NamedQuery(name="stakeHolder.findall",query="SELECT stkhold FROM StakeHolder stkhold WHERE stkhold.company = 2")
+@Table(name="STAKEHOLDER")
+public class StakeHolder {
+
+	@Id
+	@TableGenerator(name = "StakeHolder_GEN", table = "ID_GEN", pkColumnName = "GEN_NAME", valueColumnName = "GEN_VAL", allocationSize = 1)
+	@GeneratedValue(strategy= GenerationType.TABLE, generator = "StakeHolder_GEN")
+	private long id;
+	
+	@ManyToOne
+	private CompanyDetails company;
+	
+	
+	private String stakeHolderName;
+	
+	private Date dob;
+	
+	private String address;
+	
+	private long phNo;
+	
+	private String email;
+	
+	private String panNo;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public CompanyDetails getCompany() {
+		return company;
+	}
+
+	public void setCompany(CompanyDetails company) {
+		this.company = company;
+	}
+
+	public String getStakeHolderName() {
+		return stakeHolderName;
+	}
+
+	public void setStakeHolderName(String stakeHolderName) {
+		this.stakeHolderName = stakeHolderName;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public long getPhNo() {
+		return phNo;
+	}
+
+	public void setPhNo(long phNo) {
+		this.phNo = phNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPanNo() {
+		return panNo;
+	}
+
+	public void setPanNo(String panNo) {
+		this.panNo = panNo;
+	}
+	
+}
